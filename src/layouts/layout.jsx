c24fb1d66d9979c7fcf62223deab64e370179d99
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2023-07-31 17:28:18 +0100

    Add main content padding

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <main className="container mx-auto flex-grow">
+            <main className="container mx-auto p-4 md:p-6 flex-grow">
                 <Outlet />
             </main>
             <Footer />
