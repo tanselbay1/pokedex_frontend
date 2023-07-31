b755b82a5d3a39b9e51abb09e9ba45fe0979eb60
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2023-07-31 15:31:25 +0100

    Add home link to logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const navigation = [
     { name: 'Pokedex', href: '/', current: true },
@@ -39,11 +39,13 @@ export default function Navbar() {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
-                                    <img
-                                        className="h-10 w-auto"
-                                        src="https://cdn2.vectorstock.com/i/1000x1000/73/91/pokemon-logo-icon-template-vector-23237391.jpg"
-                                        alt="Your Company"
-                                    />
+                                    <Link to="/">
+                                        <img
+                                            className="h-10 w-auto"
+                                            src="https://cdn2.vectorstock.com/i/1000x1000/73/91/pokemon-logo-icon-template-vector-23237391.jpg"
+                                            alt="Your Company"
+                                        />
+                                    </Link>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
