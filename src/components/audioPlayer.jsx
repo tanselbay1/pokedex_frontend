6173daf7fd89c6be34bd2a18870106648c4030ae
tanselbay1 <tanselbay1@hotmail.com>
2023-08-03 15:09:40 +0700

    Add secret sound to the bell icon

---
+++ src/components/audioPlayer.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { BellIcon } from '@heroicons/react/24/outline';
+
+const useAudio = url => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = ({ url }) => {
+  const [playing, toggle] = useAudio(url);
+
+  return (
+      <button
+         onClick={toggle}
+         type="button"
+         className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+      >
+         <BellIcon className="h-6 w-6"
+                  aria-hidden="true"/>
+      </button>
+      
+  );
+};
+
+export default Player;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
 import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, NavLink } from 'react-router-dom';
+import Player from "./audioPlayer";
+import pokeSound from "../assets/audio/gotta-catch-em-all.mp3";
 
 const navigation = [
     { name: 'Pokedex', href: '/' },
@@ -58,18 +60,21 @@ export default function Navbar() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                >
-                                    <span className="sr-only">
+                            <Player url={pokeSound}/>
+                                
+                                    {/* <span className="sr-only">
                                         View notifications
-                                    </span>
-                                    <BellIcon
+                                    </span> */}
+
+
+                                    {/* <BellIcon
                                         className="h-6 w-6"
                                         aria-hidden="true"
-                                    />
-                                </button>
+                                    /> */}
+                                    {/* </Player> */}
+                                    
+                                
+                           {/* </Player> */}
                             </div>
                         </div>
                     </div>
